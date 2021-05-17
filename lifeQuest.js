3cf272170f7ae24156ad62d4de78ef325d23255b
thecodingbirds <thecodingbirds@gmail.com>
2021-05-17 21:52:20 +0530

    made a new branch

--- lifeQuest.js
+++ lifeQuest.js
@@ -10,7 +10,7 @@ Each activity needs to completed everyday for the TIME PERIOD of the level.
 
 :: SIDE QUESTS -> These are the activities which can be done to get quick rewards. These are not needed to upgrade to the next level.
 
-REWARD : for eac level an award will be set and given on the completion of the level.
+REWARD : for each level an award will be set and given on the completion of the level.
 
 If the time period is completed then the app will check the activities done and then upgrade/repeat/degrade to the respective level.
 
@@ -18,3 +18,8 @@ Upgrade to the Next level -> If the main Objective is completed & DailySubMissio
 Repeat the level -> If only one of the mainObjective or DailySubMissions is completed.
 Degrade the level -> If neither of the mainObjective & DailySubmissions is completed.
 */
+
+var level = prompt("What level is this?")
+print(`&level`);
+var mObj = prompt("What is the main Objective that you want to set for this level")
+print(`&mObj`);
